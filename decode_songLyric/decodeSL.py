0d53cef5340943881f00bcbaa9b683b9d90c47cd
Christian Sacchi <32966583+christiansacchi@users.noreply.github.com>
2017-10-31 17:32:41 +0100

    Update decodeSL.py

--- decode_songLyric/decodeSL.py
+++ decode_songLyric/decodeSL.py
@@ -1,34 +1,69 @@
 # -*- coding: utf-8 -*-
 
-# decode_songLyric.py - 2017
+# createDB.py - 2017 
 
-import io
+import sqlite3 as lite
 
-lyrc = io.open('caparezza_01.txt', mode='r', encoding='utf-8')
+conn = lite.connect('decodeSL.db')
+c = conn.cursor()
 
-wordRef = {}
+createTable_autore = ''' CREATE TABLE 'autore' (
+	'id' int(10) NOT NULL,
+	'nome' varchar(128) NOT NULL,
+	'cognome' varchar(128) DEFAULT NULL,
+	'anno' year(4) NOT NULL,
 
-for l in lyrc:
-	_l = l.split()
+	PRIMARY KEY ('id')
+); ''';
 
-	for w in _l:
-		
-		if (w == '---'):
-			continue
+createTable_parola = ''' CREATE TABLE 'parola' (
+	'id' bigint(20) NOT NULL,
+	'word' varchar(256) NOT NULL,
+	'lingua' varchar(128) NOT NULL,
+	'len' int(10) NOT NULL,
 
-		w = unicode(w)
-		w = w.lower()
-		for ch in [' ', '\n', '\"', '\'', ',', '(', ')', ':', ';', '.', '!', '?']:
-			w = w.strip(ch)
+	PRIMARY KEY ('id')
+); ''';
 
-		if '\'' in w:
-			w = w.split('\'')[1]
+createTable_raccolta = ''' CREATE TABLE 'raccolta' (
+	'id' int(10) NOT NULL,
+	'nome' varchar(256) NOT NULL,
+	'tipo' varchar(256) NOT NULL,
 
-		if w in wordRef:
-			wordRef[w] += 1
-		else:
-			wordRef[w] = 1
+	PRIMARY KEY ('id')
+); ''';
 
-for w, c in wordRef.items():
-	w = unicode(w)
-	print(u"{0}: {1}".format(w, c))
+createTable_testo = ''' CREATE TABLE 'testo' (
+	'id' int(10) NOT NULL,
+	'lyrics' text,
+	'anno' year(4) NOT NULL,
+	'id_raccolta' int(10) NOT NULL,
+	'id_autore' int(10) NOT NULL,
+
+	PRIMARY KEY ('id'),
+	FOREIGN KEY ('id_raccolta') REFERENCES raccolta('id'),
+	FOREIGN KEY ('id_autore') REFERENCES autore('id')
+); ''';
+
+createTable_composizione = ''' CREATE TABLE 'composizione' (
+	'id_testo' int(10) NOT NULL,
+	'id_parola' bigint(20) NOT NULL,
+	'frequenza' int(10) NOT NULL,
+
+	CONSTRAINT 'id_composizione' PRIMARY KEY ('id_testo','id_parola'),
+	FOREIGN KEY ('id_testo') REFERENCES testo('id'),
+	FOREIGN KEY ('id_parola') REFERENCES parola('id')
+); ''';
+
+c.execute(createTable_autore)
+c.execute(createTable_parola)
+c.execute(createTable_raccolta)
+c.execute(createTable_testo)
+c.execute(createTable_composizione)
+
+c.execute('''INSERT INTO 'raccolta' ('id', 'nome', 'tipo') VALUES (1, 'singolo', 'album');''')
+c.execute('SELECT * FROM raccolta')
+print (c.fetchone())
+
+conn.commit()
+conn.close()
