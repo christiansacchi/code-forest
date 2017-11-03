0b2f675fe6a476e82e3e21855ecb976d7f2f5a15
Christian Sacchi <32966583+christiansacchi@users.noreply.github.com>
2017-11-03 17:11:03 +0100

    Update wordreference_request.py

--- decode_songLyric/wordreference_request.py
+++ decode_songLyric/wordreference_request.py
@@ -4,80 +4,82 @@ import xml.etree.ElementTree as tree
 
 import html5lib # pip install html5lib
 
-# -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- #
+class WordReference_word_validation (object):
 
-def find(tree, tag):
-	for i in tree.iter():
-		if (type(i.tag) is str):
-			_tag = i.tag[len('{http://www.w3.org/1999/xhtml}'):]
-			if _tag == tag:
-					return i
-def find_byId(tree, tag, id):
-	for i in tree.iter():
-		if (type(i.tag) is str):
-			_tag = i.tag[len('{http://www.w3.org/1999/xhtml}'):]
-			if _tag == tag:
-				if 'id' in i.attrib:
-					if i.attrib['id'] == id:
+	def __init__ (self):
+		pass
+
+	def find(self, tree, tag):
+		for i in tree.iter():
+			if (type(i.tag) is str):
+				_tag = i.tag[len('{http://www.w3.org/1999/xhtml}'):]
+				if _tag == tag:
 						return i
-def find_byClass(tree, tag, clas):
-	for i in tree.iter():
-		if (type(i.tag) is str):
-			_tag = i.tag[len('{http://www.w3.org/1999/xhtml}'):]
-			if _tag == tag:
-				if 'class' in i.attrib:
-					if i.attrib['class'] == clas:
-						return i
+	def find_byId(self, tree, tag, id):
+		for i in tree.iter():
+			if (type(i.tag) is str):
+				_tag = i.tag[len('{http://www.w3.org/1999/xhtml}'):]
+				if _tag == tag:
+					if 'id' in i.attrib:
+						if i.attrib['id'] == id:
+							return i
+	def find_byClass(self, tree, tag, clas):
+		for i in tree.iter():
+			if (type(i.tag) is str):
+				_tag = i.tag[len('{http://www.w3.org/1999/xhtml}'):]
+				if _tag == tag:
+					if 'class' in i.attrib:
+						if i.attrib['class'] == clas:
+							return i
 
-# -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- #
-t = None;
-site = 'http://www.wordreference.com/{0}'
-sitePos = '{0}/{1}'
-land = 'iten'
-theWord = 'just'
-page = req.get(site.format(sitePos.format(land, theWord)), allow_redirects=False, verify=False)
+	def isword (self, word):
 
-f = open('lol.html', 'wb')
-f.write(page.content)
-f.close()
+		_OUT = None
 
-# -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- -=-=-=-=-=-=-=-=-=-=-=- #
+		t = None;
+		site = 'http://www.wordreference.com/{0}'
+		sitePos = '{0}/{1}'
+		land = 'iten'
+		textPag = "lol.html"
+		page = req.get(site.format(sitePos.format(land, word)), allow_redirects=False, verify=False)
 
-htmlf = open('lol.html', 'rb')
-html = html5lib.parse(htmlf)
+		f = open(textPag, 'wb')
+		f.write(page.content)
+		f.close()
 
-title = find( find(html, 'head'), 'title')
-if title.text == 'Object moved':
-	sitePos = find( find(html, 'body'), 'a').attrib['href']
-	site = 'http://www.wordreference.com/{0}'.format(sitePos)
-	page = req.get(site.format(sitePos.format(land, theWord)), allow_redirects=False, verify=False)
-	f = open('lol.html', 'wb')
-	f.write(page.content)
-	f.close()
+		htmlf = open(textPag, 'rb')
+		html = html5lib.parse(htmlf)
 
-	htmlf = open('lol.html', 'rb')
-	html = html5lib.parse(htmlf)
+		title = self.find( self.find(html, 'head'), 'title')
+		if title.text == 'Object moved':
+			sitePos = self.find( self.find(html, 'body'), 'a').attrib['href']
+			site = 'http://www.wordreference.com/{0}'.format(sitePos)
+			page = req.get(site.format(sitePos.format(land, word)), allow_redirects=False, verify=False)
 
-body = find(html, 'body')
-print (body)
+			f = open(textPag, 'wb')
+			f.write(page.content)
+			f.close()
 
-table = find_byId(body, 'table', 'contenttable')
-print (table)
+			htmlf = open(textPag, 'rb')
+			html = html5lib.parse(htmlf)
 
-elmt = find_byId(table, 'td', 'centercolumn')
-print (elmt)
+		body = self.find(html, 'body')
+		table = self.find_byId(body, 'table', 'contenttable')
+		elmt = self.find_byId(table, 'td', 'centercolumn')
 
-elmt = find_byId(elmt, 'div', 'articleWRD')
-print (elmt)
+		noEntryFound = self.find_byId(elmt, 'p', 'noEntryFound')
+		if noEntryFound is not None:
+			return word
 
-elmt = find_byClass(elmt, 'table', 'WRD')
-print (elmt)
+		elmt = self.find_byId(elmt, 'div', 'articleWRD')
+		if len(elmt) == 0:
+			'''sitePos = '{0}/reverse/{1}'
+			page = req.get(site.format(sitePos.format(land, word)), allow_redirects=False, verify=False)'''
+			return word
 
-elmt = find_byClass(elmt, 'tr', 'even')
-print (elmt.attrib)
-print (elmt)
+		elmt = self.find_byClass(elmt, 'table', 'WRD')
+		elmt = self.find_byClass(elmt, 'tr', 'even')
+		elmt = self.find_byClass(elmt, 'td', 'FrWrd')
+		elmt = self.find(elmt, 'strong')
 
-elmt = find_byClass(elmt, 'td', 'FrWrd')
-print (elmt.attrib)
-elmt = find(elmt, 'strong')
-print(elmt.text)
+		return elmt.text
